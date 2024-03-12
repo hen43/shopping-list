cc99ec75d2be2478c6f5939b17d76f8c085f2020
Henry Le <140691753+hen43@users.noreply.github.com>
2024-03-12 18:09:26 +0000

    exceptions

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ items = []
 
 # TEST LIST
 
-items.append({
-  'name': 'Fortnite Burger', 
-  'quantity': 9001, 
-  'price': 32.32
-})
+# items.append({
+#   'name': 'Fortnite Burger', 
+#   'quantity': 9001, 
+#   'price': 32.32
+# })
 
-items.append({
-  'name': 'Burger King Foot Lettuce', 
-  'quantity': 15, 
-  'price': 0.01,
-})
+# items.append({
+#   'name': 'Burger King Foot Lettuce', 
+#   'quantity': 15, 
+#   'price': 0.01,
+# })
 
 # VERY EPIC GUIDE ON USING DICTIONARIES
 
@@ -32,13 +32,24 @@ def viewList():
     print(f'''
 Item: {i['name']}
 Quantity: {i['quantity']}
-Price: ${i['price']}
-Total Cost: ${i['quantity'] * i['price']}
-    ''')
+Price: ${'{:.2f}'.format(i['price'])}
+Total Cost: ${'{:.2f}'.format(i['quantity'] * i['price'])}
+      ''')
+  if len(items) < 1:
+    print('\nShopping list is empty!')
 
 def removeItem(item):
-  items.pop(item)
-
+  try:
+    items.pop(item)
+  except IndexError:
+    print('\nNo item in this position.')
+def addItem(name, quantity, price):  
+  items.append({
+    'name': name, 
+    'quantity': quantity, 
+    'price': price,
+  })
+  
 def navigate(choice):
   if choice == 0:
     try:
@@ -51,24 +62,24 @@ SHOPPING LIST
 
 '''))
     except ValueError:
-      print('that aint an option bubba')
+      print('\nthat aint an option bubba')
+    
     if choice == 1: # ADD ITEM TO LiST
       name = input('what\'s the item name??')
       quantity = int(input('how many are you buying?'))
       price = float(input('how much does it cost in dollars?'))
-
-      items.append({
-        'name': name, 
-        'quantity': quantity, 
-        'price': price,
-      })
+      addItem(name, quantity, price)
+      
     elif choice == 2: # REMOVE ITEM FROM LIST
       remove = int(input('which item to remove????'))
       removeItem(remove)
+    
     elif choice == 3: # VIEW LIST
       viewList()
+    
     else:
       navigate(choice)
+      print('\nthat aint an option bubba')
 
 while stopProject == 0:
     navigate(choice)
